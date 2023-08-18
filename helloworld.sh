2744fd67c651f096adca21b9c12345776b773ac3
anoop41 <anoopchowdary41@gmail.com>
2023-08-19 02:29:58 +0530

    changes done

--- helloworld.sh
+++ helloworld.sh
@@ -1,8 +1,8 @@
 #!bin/bash
 
 echo"Hello world "
-person1=ramesh
-person2=suresh
+person1=venkata
+person2=anoop
 echo "$person1: hi $person2  good morning"
 echo " $person2 : good morning"
 echo "$person1: how are you $person2"
