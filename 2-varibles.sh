44b0e0825c4367253e85684e0cd5ae196338cfba
anoop41 <anoopchowdary41@gmail.com>
2023-08-19 02:43:44 +0530

    changes done

---
+++ 2-varibles.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+
+echo"Hello world "
+person1=$1
+person2=$2
+echo "$person1: hi $person2  good morning"
+echo " $person2 : good morning"
+echo "$person1: how are you $person2"
+echo "$person2: am good how are you $person1"
