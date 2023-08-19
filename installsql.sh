c7ecc20211a0eb6a04824a6cc17e1ef7ebca969e
anoop41 <anoopchowdary41@gmail.com>
2023-08-19 20:00:47 +0530

    changes done

--- installsql.sh
+++ installsql.sh
@@ -3,7 +3,7 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0]
 then 
-echo "error ::please run this scripit in root user"
+echo "error:: please run this scripit in root user"
 exit 1
 fi 
 yum install mysql -y
